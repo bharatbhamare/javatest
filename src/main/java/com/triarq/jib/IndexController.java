43b808294be7f5339b2fcab657d4870b4a4f03a1
bharatbhamare <b3.aug95@gmail.com>
2019-09-26 00:05:22 +0530

    first commit

---
+++ src/main/java/com/triarq/jib/IndexController.java
@@ -0,0 +1,15 @@
+package com.triarq.jib;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+	
+	@GetMapping("/")
+	public String sayHello()
+	{
+		return "hello gcp";
+	}
+}
---
+++ src/test/java/com/triarq/jib/JavatestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.triarq.jib;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavatestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
